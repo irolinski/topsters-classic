c661157f09ac01c8103202200fef51a0a2f3511f
szczek <ignacy.rolinski@gmail.com>
2024-06-28 16:01:32 +0200

    feat: create separate canvas UI for user; feat: handle no data;

--- src/Components/Charts/Collage.tsx
+++ src/Components/Charts/Collage.tsx
@@ -14,63 +14,124 @@ const Collage = ({
   changeIndex,
 }: CollageProps) => {
   return (
-    <div
-      className="collage html2canvas-container w-full p-[2px] content-center"
-      ref={exportRef}
-    >
-      <div className="w-3/5">
-        <div className="image-div flex justify-center top-1/2 max-h-full max-w-2/3 flex-wrap m-auto -translate-x-[20px]">
-          {collageData.map((a, i) => {
-            return (
-              <div
-                className={`${
-                  i === selectedIndex && "selected-index"
-                } collage w-[125px] h-[125px] m-[2px]`}
-                key={i}
-                onClick={() => {
-                  changeIndex(i);
-                  //   setSelectedIndex(i);
-                }}
-              >
-                {/* remmber to delete "#text" later */}
-                {
-                  /*@ts-ignore */
-                  a.image[1]["#text"] ? (
-                    <img
-                      className="w-full "
-                      /*@ts-ignore */
-                      src={`${a.image[2]["#text"]}`}
-                    />
+    <>
+      {/* canvas UI */}
+      <div
+        className="collage-container w-full p-[2px] content-center"
+        ref={exportRef}
+      >
+        <div className="w-3/5">
+          <div className="image-div flex justify-center top-1/2 max-h-full max-w-2/3 flex-wrap m-auto -translate-x-[20px]">
+            {collageData.map((a, i) => {
+              return (
+                <div
+                  className={`${
+                    i === selectedIndex && "selected-index"
+                  } collage w-[125px] h-[125px] m-[2px]`}
+                  key={i}
+                  onClick={() => {
+                    changeIndex(i);
+                    //   setSelectedIndex(i);
+                  }}
+                >
+                  {/* remmber to delete "#text" later */}
+                  {a.hasOwnProperty("image") ? (
+                    /*@ts-ignore */
+                    a.image[1]["#text"] ? (
+                      <img
+                        className="w-full "
+                        /*@ts-ignore */
+                        src={`${a.image[2]["#text"]}`}
+                      />
+                    ) : (
+                      <img className="w-full " src={`${a.image[2]["text"]}`} />
+                    )
                   ) : (
-                    <img className="w-full " src={`${a.image[2]["text"]}`} />
-                  )
-                }
-              </div>
-            );
-          })}
+                    <div className="w-full h-full bg-gray"> </div>
+                  )}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+
+        <div className="w-2/5 max-h-full">
+          <div className="collage leading-none -translate-x-[40px]">
+            {collageData.map((a, i) => {
+              return a.artist ? (
+                <>
+                  <span className="m-[2px] text-left">
+                    {a.artist} - {a.name}{" "}
+                  </span>
+                  {(i + 1) % 4 === 0 && (
+                    <div>
+                      {" "}
+                      <br />{" "}
+                    </div>
+                  )}
+                </>
+              ) : (
+                <div className="w-full h-full bg-gray"></div>
+              );
+            })}
+          </div>
         </div>
       </div>
 
-      <div className="w-2/5 max-h-full">
-        <div className="collage leading-none -translate-x-[40px]">
-          {collageData.map((a, i) => {
-            return (
-              <>
-                <span className="m-[2px] text-left">
-                  {a.artist} - {a.name}{" "}
-                </span>
-                {(i + 1) % 4 === 0 && (
-                  <div>
-                    {" "}
-                    <br />{" "}
-                  </div>
-                )}
-              </>
-            );
-          })}
+      {/* actual canvas */}
+      <div
+        className="collage-container html2canvas-container w-full p-[2px] content-center"
+        ref={exportRef}
+      >
+        <div className="w-3/5">
+          <div className="image-div flex justify-center top-1/2 max-h-full max-w-2/3 flex-wrap m-auto -translate-x-[20px]">
+            {collageData.map((a, i) => {
+              return (
+                <div className="collage w-[125px] h-[125px] m-[2px]" key={i}>
+                  {/* remmber to delete "#text" later */}
+                  {a.hasOwnProperty("image") ? (
+                    /*@ts-ignore */
+                    a.image[1]["#text"] ? (
+                      <img
+                        className="w-full "
+                        /*@ts-ignore */
+                        src={`${a.image[2]["#text"]}`}
+                      />
+                    ) : (
+                      <img className="w-full " src={`${a.image[2]["text"]}`} />
+                    )
+                  ) : (
+                    <div className="w-full h-full bg-gray"> </div>
+                  )}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+
+        <div className="w-2/5 max-h-full">
+          <div className="collage leading-none -translate-x-[40px]">
+            {collageData.map((a, i) => {
+              return (
+                a.artist && (
+                  <>
+                    <span className="m-[2px] text-left">
+                      {a.artist} - {a.name}{" "}
+                    </span>
+                    {(i + 1) % 4 === 0 && (
+                      <div>
+                        {" "}
+                        <br />{" "}
+                      </div>
+                    )}
+                  </>
+                )
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ src/assets/lots_of_mahler.js
@@ -0,0 +1,322 @@
+let collage = [
+    {
+        name: "Mahler: Symphony No. 2",
+        artist: "Gustav Mahler",
+        image: [
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/34s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "small",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/64s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "medium",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/174s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "large",
+            },
+        ],
+        streamable: "0",
+        url: "https://www.last.fm/music/Gustav+Mahler/Mahler:+Symphony+No.+2",
+    },
+    {
+        name: "Mahler: Symphony No. 2",
+        artist: "Gustav Mahler",
+        image: [
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/34s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "small",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/64s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "medium",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/174s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "large",
+            },
+        ],
+        streamable: "0",
+        url: "https://www.last.fm/music/Gustav+Mahler/Mahler:+Symphony+No.+2",
+    },
+    {
+        name: "Mahler: Symphony No. 2",
+        artist: "Gustav Mahler",
+        image: [
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/34s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "small",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/64s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "medium",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/174s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "large",
+            },
+        ],
+        streamable: "0",
+        url: "https://www.last.fm/music/Gustav+Mahler/Mahler:+Symphony+No.+2",
+    },
+    {
+        name: "Mahler: Symphony No. 2",
+        artist: "Gustav Mahler",
+        image: [
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/34s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "small",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/64s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "medium",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/174s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "large",
+            },
+        ],
+        streamable: "0",
+        url: "https://www.last.fm/music/Gustav+Mahler/Mahler:+Symphony+No.+2",
+    },
+    {
+        name: "Mahler: Symphony No. 2",
+        artist: "Gustav Mahler",
+        image: [
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/34s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "small",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/64s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "medium",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/174s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "large",
+            },
+        ],
+        streamable: "0",
+        url: "https://www.last.fm/music/Gustav+Mahler/Mahler:+Symphony+No.+2",
+    },
+    {
+        name: "Mahler: Symphony No. 2",
+        artist: "Gustav Mahler",
+        image: [
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/34s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "small",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/64s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "medium",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/174s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "large",
+            },
+        ],
+        streamable: "0",
+        url: "https://www.last.fm/music/Gustav+Mahler/Mahler:+Symphony+No.+2",
+    },
+    {
+        name: "Mahler: Symphony No. 2",
+        artist: "Gustav Mahler",
+        image: [
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/34s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "small",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/64s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "medium",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/174s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "large",
+            },
+        ],
+        streamable: "0",
+        url: "https://www.last.fm/music/Gustav+Mahler/Mahler:+Symphony+No.+2",
+    },
+    {
+        name: "Mahler: Symphony No. 2",
+        artist: "Gustav Mahler",
+        image: [
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/34s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "small",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/64s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "medium",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/174s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "large",
+            },
+        ],
+        streamable: "0",
+        url: "https://www.last.fm/music/Gustav+Mahler/Mahler:+Symphony+No.+2",
+    },
+    {
+        name: "Mahler: Symphony No. 2",
+        artist: "Gustav Mahler",
+        image: [
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/34s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "small",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/64s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "medium",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/174s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "large",
+            },
+        ],
+        streamable: "0",
+        url: "https://www.last.fm/music/Gustav+Mahler/Mahler:+Symphony+No.+2",
+    },
+    {
+        name: "Mahler: Symphony No. 2",
+        artist: "Gustav Mahler",
+        image: [
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/34s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "small",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/64s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "medium",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/174s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "large",
+            },
+        ],
+        streamable: "0",
+        url: "https://www.last.fm/music/Gustav+Mahler/Mahler:+Symphony+No.+2",
+    },
+    {
+        name: "Mahler: Symphony No. 2",
+        artist: "Gustav Mahler",
+        image: [
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/34s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "small",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/64s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "medium",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/174s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "large",
+            },
+        ],
+        streamable: "0",
+        url: "https://www.last.fm/music/Gustav+Mahler/Mahler:+Symphony+No.+2",
+    },
+    {
+        name: "Mahler: Symphony No. 2",
+        artist: "Gustav Mahler",
+        image: [
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/34s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "small",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/64s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "medium",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/174s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "large",
+            },
+        ],
+        streamable: "0",
+        url: "https://www.last.fm/music/Gustav+Mahler/Mahler:+Symphony+No.+2",
+    },
+    {
+        name: "Mahler: Symphony No. 2",
+        artist: "Gustav Mahler",
+        image: [
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/34s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "small",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/64s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "medium",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/174s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "large",
+            },
+        ],
+        streamable: "0",
+        url: "https://www.last.fm/music/Gustav+Mahler/Mahler:+Symphony+No.+2",
+    },
+    {
+        name: "Mahler: Symphony No. 2",
+        artist: "Gustav Mahler",
+        image: [
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/34s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "small",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/64s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "medium",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/174s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "large",
+            },
+        ],
+        streamable: "0",
+        url: "https://www.last.fm/music/Gustav+Mahler/Mahler:+Symphony+No.+2",
+    },
+    {
+        name: "Mahler: Symphony No. 2",
+        artist: "Gustav Mahler",
+        image: [
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/34s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "small",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/64s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "medium",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/174s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "large",
+            },
+        ],
+        streamable: "0",
+        url: "https://www.last.fm/music/Gustav+Mahler/Mahler:+Symphony+No.+2",
+    },
+    {
+        name: "Mahler: Symphony No. 2",
+        artist: "Gustav Mahler",
+        image: [
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/34s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "small",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/64s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "medium",
+            },
+            {
+                text: "https://lastfm.freetls.fastly.net/i/u/174s/e89aea1eebf0480a073fa63004dcc804.png",
+                size: "large",
+            },
+        ],
+        streamable: "0",
+        url: "https://www.last.fm/music/Gustav+Mahler/Mahler:+Symphony+No.+2",
+    },
+];
