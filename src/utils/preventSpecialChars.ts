0374d421d06f900ce8f6c35b8e63bbd427138e6a
szczek <ignacy.rolinski@gmail.com>
2024-07-22 14:03:45 +0200

    feat: add alert that is to be confirmed before delete;

--- src/utils/preventSpecialChars.ts
+++ src/utils/preventSpecialChars.ts
@@ -1,6 +1,6 @@
 
 export const preventSpecialChar = (evt: any) => {
-  if (!/[A-Za-z0-9\s_,. ()?!]/.test(evt.key)) {
+  if (!/[A-Za-z0-9\s_,. ()?!-]/.test(evt.key)) {
     evt.preventDefault();
   }
 };
